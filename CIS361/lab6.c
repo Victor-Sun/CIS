82a91f5cbc46954a22f77ec0d41750c55a7da24f
Victor <mynameisvictor@126.com>
2016-03-15 15:04:31 -0400

    361

--- CIS361/lab6.c
+++ CIS361/lab6.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define MAXLINE 80
 #define MAXARG 20
@@ -9,63 +11,63 @@ void background (char * cmd);
 
 
 int main(){
-    char cmd[MAXLINE];
+	char cmd[MAXLINE];
 
-    for (; ;) {
-        printf("mysh$ ");
-        fgets(cmd, MAXLINE, stdin); 
-        // read a command from the user
-        // if the command in cmd is “exit\n”, then terminate this program;
-        // otherwise, create a child process to handle that command.
+	for (; ;) {
+		printf("mysh$ ");
+		fgets(cmd, MAXLINE, stdin); 
+		// read a command from the user
+		// if the command in cmd is “exit\n”, then terminate this program;
+		// otherwise, create a child process to handle that command.
 
-        if (strcmp(cmd, "exit\n") == 0) {
-            break;
-        } else {
-            background(cmd);
-        }
-    }
+		if (strcmp(cmd, "exit\n") == 0) {
+			break;
+		} else {
+			background(cmd);
+		}
+	}
 
-    return 0;
+	return 0;
 }
 
 void background (char * cmd){
-    int i;
-    char *argv[MAXARG];
-    const char DELIMITERS[] = "\t \n\r";
+	int i;
+	pid_t pid;
+	char *argv[MAXARG];
+	const char DELIMITERS[] = "\t \n\r";
 
-    argv[0] = strtok(cmd, DELIMITERS);
+	argv[0] = strtok(cmd, DELIMITERS);
 
-    for (i = 1; i < MAXARG - 1; i++) {
-        argv[i] = strtok(NULL, DELIMITERS);
-        if (argv[i] == NULL) {
-            break;
-        }
-    }
-    argv[i] = NULL;
-    
-    printf("cmd = %p \"%s\"\n", cmd, cmd);
-    for (i = 0; i < MAXARG; i++) {
-        if (argv[i] == NULL) {
-            printf("argv[%d] = %p \n", i, argv[i]);
-            break;
-        } else {
-            printf("argv[%d] = %p \"%s\"\n", i, argv[i], argv[i]);
-        }
-    }
-    
-    
-    // cmd = "ls -l /tmp\n"
+	for (i = 1; i < MAXARG - 1; i++) {
+		argv[i] = strtok(NULL, DELIMITERS);
+		if (argv[i] == NULL) {
+			break;
+		}
+	}
+	argv[i] = NULL;
 
-    // cmd = "ls\0-l\0/tmp\0"
+	/*
+	printf("cmd = %p \"%s\"\n", cmd, cmd);
+	for (i = 0; i < MAXARG; i++) {
+		if (argv[i] == NULL) {
+			printf("argv[%d] = %p \n", i, argv[i]);
+			break;
+		} else {
+			printf("argv[%d] = %p \"%s\"\n", i, argv[i], argv[i]);
+		}
+	}
+	*/
 
-    // argv[0] -> "ls"  -> cmd + 0
-    // argv[1] -> "-l" -> cmd + 3
-    // argv[2] -> "/tmp"
-    // argv[3] -> NULL
-    
+	pid = fork();
 
-    // int execvp(const char *file, char *const argv[]);
-
-    //execvp (argv[0], argv);
-
+	if (pid < 0) {
+		perror("Error while forking");
+		return;
+	} else if (pid == 0) {
+		execvp(argv[0], argv);
+		perror("Error when executing the command.");
+	} else {
+		int status;
+		waitpid(pid, &status, 0);
+	}
 }

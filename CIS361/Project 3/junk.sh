0c545c8061a3d052030b4af3d7ebcc0159fbdac8
Victor Sun <mynameisvictor@126.com>
2016-04-08 11:54:16 -0400

    Junk

--- CIS361/Project 3/junk.sh
+++ CIS361/Project 3/junk.sh
@@ -4,7 +4,7 @@ currDir=$(pwd)
 junkDir="~/.junk"
 
 show_help(){
-	cat <<EOF
+    cat <<EOF
 TODO: Help message + Command options + Usage
 EOF
 }
@@ -12,53 +12,53 @@ EOF
 
 
 if [ $# -eq 0 ]
-	then
-		echo "Not enough arguments, use --help to see usage."
-		exit s1
+    then
+        echo "Not enough arguments, use --help to see usage."
+        exit s1
 fi
 
 for cmdArg in $*
 do
-	case $cmdArg in
-		"-l") optionL=1;;
-		"-n") optionN=1;;
-		"--help") optionH=1;;
-		*) list="$list $cmdArg";;
-	esac
+    case $cmdArg in
+        "-l") optionL=1;;
+        "-n") optionN=1;;
+        "--help") optionH=1;;
+        *) list="$list $cmdArg";;
+    esac
 done
 
 mkdir -p $junkDir
 
-if [ $optionL = 1 ]
-	then
-		F=$(ls -al $junkDir)
-		echo "Files inside junk directory: $F"
+if [ "$optionL" = "1" ]
+    then
+        F=$(ls -al $junkDir)
+        echo "Files inside junk directory: $F"
 fi
 
-if [ $optionN = 1 ]
-	then
-		N=$(find $junkDir -type f | wc -l)
-		echo "Number of files in junk directory: $N"
+if [ "$optionN" = "1" ]
+    then
+        N=$(find $junkDir -type f | wc -l)
+        echo "Number of files in junk directory: $N"
 fi
 
-if [ $optionH = 1 ]
-	then
-		show_help
+if [ "$optionH" = "1" ]
+    then
+        show_help
 fi
 
 if [ -z $list ]
-	then 
-		for file in $list
-		do
-			if [ -d $file ]
-				echo "Directories are now allowed to be moved!"
-				echo "Use --help for usage information."
-			else
-				if [ -f $file ]
-					then
-						mv $file $junkDir
-				fi
-			fi
-		done
-	exit 0
+    then 
+        for file in $list
+        do
+            if [ -d $file ]
+                echo "Directories are now allowed to be moved!"
+                echo "Use --help for usage information."
+            else
+                if [ -f $file ]
+                    then
+                        mv $file $junkDir
+                fi
+            fi
+        done
+    exit 0
 fi

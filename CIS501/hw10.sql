8e3131ddae626b5fcd9f960bd860adc5512a2095
Victor Sun <mynameisvictor@126.com>
2016-04-14 12:42:11 -0400

    z

---
+++ CIS501/hw10.sql
@@ -0,0 +1,24 @@
+--
+-- File: hw10.sql
+-- Name: Victor Sun
+--
+
+CREATE TABLE artist (
+	id number PRIMARY KEY,
+	fname varchar2(15),
+	lname varchar2(15),
+);
+--
+
+CREATE TABLE albums (
+	id
+	title
+	release_date
+);
+--
+
+CREATE TABLE tracks (
+	id
+	title
+);
+--

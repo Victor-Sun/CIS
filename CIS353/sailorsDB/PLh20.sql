b5e2a4ca90c49cd1ef56da50d902e6ba76c70845
Victor <mynameisvictor@126.com>
2016-04-16 06:24:20 -0400

    z

--- CIS353/sailorsDB/PLh20.sql
+++ CIS353/sailorsDB/PLh20.sql
@@ -1,5 +1,5 @@
 -- File PLh20.sql
--- Author: <<< YOUR NAME >>>
+-- Author: VICTOR SUN
 -------------------------------------------------------------------
 SET SERVEROUTPUT ON
 SET VERIFY OFF
@@ -7,8 +7,46 @@ SET VERIFY OFF
 ACCEPT rateDecrement NUMBER PROMPT 'Enter the rate decrement: '
 ACCEPT allowedMinRate NUMBER PROMPT 'Enter the allowed min. rate: '
 DECLARE
- <your declarations go here>
+	br boats%ROWTYPE;
+	CURSOR bCursor IS
+	SELECT bid, bname, color, rate, length, logkeeper
+	FROM boats;
 BEGIN
- <your code goes here>
+	OPEN bCursor;
+	LOOP
+		-- Fetch rows
+		FETCH bCursor INTO br;
+		EXIT WHEN bCursor%NOTFOUND;
+		
+		-- Print old boat rating
+		DBMS_OUTPUT.PUT_LINE ('+++++ Boat:'||br.bid||': old rate = '||br.rate);
+		br.rating := br.rating - &rateDecrement;
+		
+		--
+		DECLARE
+			belowAllowedMin EXCEPTION;
+		BEGIN
+			IF br.rating < &allowedMinRate
+			THEN RAISE belowAllowedMin;
+			ELSE UPDATE boats
+				SET rating = br.rating
+				WHERE boats.bid = br.bid;
+				-- Print new boat record
+				DBMS_OUTPUT.PUT_LINE ('----- Boat:'||br.bid||': new rate = '||br.rate);
+			END IF;
+
+		EXCEPTION
+			WHEN belowAllowedMin THEN
+				DBMS_OUTPUT.PUT_LINE ('----- Boat:'||br.bid||': Update rejected. The new rate would have been'||br.rate);
+			WHEN OTHERS THEN
+				DBMS_OUTPUT.PUT_LINE ('----- Update rejected: '||SQLCODE||'...'||SQLERRM);
+		END;
+	END LOOP;
+
+	COMMIT;
+	CLOSE bCursor;
 END;
 / 
+--
+UNDEFINE rateDecrement
+UNDEFINE allowedMinRate

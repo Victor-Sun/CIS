451b89b4a2cdfe76c06f96210c25a080fefcf53f
Victor <mynameisvictor@126.com>
2016-04-19 18:35:51 -0400

    z

--- CIS361/Project3/Proj3c.sh
+++ CIS361/Project3/Proj3c.sh
@@ -1,25 +1,52 @@
 #!/bin/bash
 
-#./Proj3c.sh <file> <directory>
+die(){
+    echo "$@"
+    exit 1
+}
 
-# Unzip downloaded file into a data directory that is named by the user under the current working directory
-# 
-
-# Create a sub-directory under the data directory for each student and name it by the student's username in the prefix
-# 
-
-if [ $# -lt 2 ]
-	then
-		echo "Not enough arguments, see --help to see usage."
-		exit 1
+if [ $# != 1 ]
+    then
+        echo 'Not enough arguments, please use:'
+        echo './Proj3c.sh <file>'
+        exit 1
 fi
 
-if [ ${2}: -4 !=  .zip ]
-	then
-		echo "2nd parameter must be a .zip file"
-		exit 1
+zipFile="$1"
+dataDir="data"
+
+if [[ "$zipFile" != *.zip ]]
+    then
+        echo "Expected a zip file, received: $zipFile"
+        exit 1
 fi
 
-mkdir -p "$3"
 
-unzip -d "$3" "$2"
+mkdir -p "$dataDir" || die "Cannot create data directory: $dataDir"
+unzip -d "$dataDir" "$zipFile" || die "Cannot unzip $zipFile"
+
+# Moving and renaming the files.
+for filename in "$dataDir"/*_attempt_*; do
+    username="$(echo "$filename" | sed 's/^.*_\([a-z0-9]\+\)_attempt_.*$/\1/')"
+    shortname="$(echo "$filename" | sed 's/^.*_attempt_[-0-9]\+_\?\(.*\)$/\1/')"
+    if [ "$username" = "$filename" ]; then
+        echo "Could not extract the username from: $filename"
+        continue
+    fi
+    if [ "$shortname" = "$filename" ]; then
+        echo "Could not extract the original filename from: $filename"
+        continue
+    fi
+
+    if [ "$shortname" = ".txt" ]; then
+        shortname="memo.txt"
+    fi
+    userdir="${dataDir}/${username}"
+    mkdir -p "$userdir" || die "Could not create directory: $userdir"
+    mv "$filename" "${userdir}/${shortname}" || die "Could not move file: $filename"
+done
+
+for userdir in "$dataDir"/*/; do
+    make -f ../../makefile -C "$userdir" \
+    && make test -f ../../makefile -C "$userdir"
+done

6fbf524be6873a142a3a28fffd87df88823cc774
Victor <mynameisvictor@126.com>
2016-04-19 19:03:50 -0400

    Code

--- CIS361/Project3/Proj3c.sh
+++ CIS361/Project3/Proj3c.sh
@@ -21,7 +21,6 @@ if [[ "$zipFile" != *.zip ]]
         exit 1
 fi
 
-
 mkdir -p "$dataDir" || die "Cannot create data directory: $dataDir"
 unzip -d "$dataDir" "$zipFile" || die "Cannot unzip $zipFile"
 
@@ -31,21 +30,21 @@ main() {
 for filename in "$dataDir"/*_attempt_*; do
     username="$(echo "$filename" | sed 's/^.*_\([a-z0-9]\+\)_attempt_.*$/\1/')"
     shortname="$(echo "$filename" | sed 's/^.*_attempt_[-0-9]\+_\?\(.*\)$/\1/')"
-    if [ "$username" = "$filename" ]; then
-        echo "Could not extract the username from: $filename"
-        continue
-    fi
-    if [ "$shortname" = "$filename" ]; then
-        echo "Could not extract the original filename from: $filename"
-        continue
-    fi
+        if [ "$username" = "$filename" ]; then
+            echo "Could not extract the username from: $filename"
+            continue
+        fi
+        if [ "$shortname" = "$filename" ]; then
+            echo "Could not extract the original filename from: $filename"
+            continue
+        fi
 
-    if [ "$shortname" = ".txt" ]; then
-        shortname="memo.txt"
-    fi
-    userdir="${dataDir}/${username}"
-    mkdir -p "$userdir" || die "Could not create directory: $userdir"
-    mv "$filename" "${userdir}/${shortname}" || die "Could not move file: $filename"
+        if [ "$shortname" = ".txt" ]; then
+            shortname="memo.txt"
+        fi
+        userdir="${dataDir}/${username}"
+        mkdir -p "$userdir" || die "Could not create directory: $userdir"
+        mv "$filename" "${userdir}/${shortname}" || die "Could not move file: $filename"
 done
 
 for userdir in "$dataDir"/*/; do

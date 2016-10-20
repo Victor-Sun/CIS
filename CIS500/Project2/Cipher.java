3f91c10295dd832ef08fa17e0cb67510471af7ae
Victor-Sun <mynameisvictor@126.com>
2016-10-20 05:16:49 -0400

    Project 2

--- CIS500/Project2/Cipher.java
+++ CIS500/Project2/Cipher.java
@@ -1,63 +1,117 @@
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.IOException;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 /**
-   This class encrypts files using the Caesar cipher.
-   For decryption, use an encryptor whose key is the 
-   negative of the encryption key.
-*/
-public class Cipher
-{
-   /**
-      Constructs a cipher object with a given key.
-      @param aKey the encryption key
-   */
-   public Cipher(int aKey)
-   {
-      key = aKey;
-   }
+ * Project 2
+ * Name: Victor Sun
+ * Class: CIS 500
+ */
 
-   /**
-      Encrypts the contents of a stream.
-      @param in the input stream
-      @param out the output stream
-   */      
-   public void encryptStream(InputStream in, OutputStream out)
-         throws IOException
-   {
-      boolean done = false;
-      while (!done)
-      {
-         int next = in.read();
-         if (next == -1) done = true;
-         else
-         {
-			char b = (char) next;
-            char c = encrypt(b);
-            out.write(c);
-         }
-      }
-   }
-
-   /**
-      Encrypts a char.
-      @param c the char to encrypt
-      @return the encrypted char
-   */
-   public char encrypt(char c)
-   {
-	 if ( key < 0 )
-		 key = key + 26;
-
-	 if ( Character.isUpperCase(c) )
-		 return (char)((c - 'A' + key) % 26 + 'A');
+public class Cipher {
 	
-	 if ( Character.isLowerCase(c) )
-		 return (char)((c - 'a' + key) % 26 + 'a');
+	private String key;
+	private int akey;
+	private char[] keyArray;
 	
-	 return c;
-   }
+	/**
+	 * Constructs a cipher object with the given key.
+	 * @param key
+	 */
+	public Cipher(String key){
+		this.key = key;
+	}
+	
+	/**
+	 * Converts string key into an encryption key
+	 */
+	public void keyConvert(){
+		keyArray = this.key.toCharArray();
+		for(int i = 0; i < keyArray.length; i++){
+			akey += keyArray[i];
+		}
+		akey = akey % 26;
+	}
+	
+	/**
+    Encrypts the contents of a stream.
+    @param in the input stream
+    @param out the output stream
+    */      
+	 public void encryptStream(InputStream in, OutputStream out)
+	       throws IOException
+	 {
+	    boolean done = false;
+	    while (!done)
+	    {
+	       int next = in.read();
+	       if (next == -1) 
+	          done = true;
+	       else
+	       {
+				char b = (char) next;
+	          char c = encrypt(b);
+	          out.write(c);
+	       }
+	    }
+	 }
+	
+	 public void decryptStream(InputStream in, OutputStream out)
+		       throws IOException
+		 {
+		    boolean done = false;
+		    while (!done)
+		    {
+		       int next = in.read();
+		       if (next == -1) 
+		          done = true;
+		       else
+		       {
+					char b = (char) next;
+		          char c = decrypt(b);
+		          out.write(c);
+		       }
+		    }
+		 }
+	 
+	/**
+	 * Encrypts the string
+	 * @param c
+	 * @return the encrypted character
+	 */
+	public char encrypt(char c){
+		 if ( akey < 0 )
+			 key = key + 26;
 
-   private int key;
+		 if (Character.isUpperCase(c) )
+			 return (char)((c - 'A' + akey) % 26 + 'A');
+		
+		 if (Character.isLowerCase(c) )
+			 return (char)((c - 'a' + akey) % 26 + 'a');
+		
+		 return c;
+	}
+	
+	/**
+	 * Decrypts the string
+	 * @param c
+	 * @return the decrypted character
+	 */
+	public char decrypt(char c){
+		if ( akey < 0 )
+			 key = key + 26;
+
+		 if (Character.isUpperCase(c) )
+			 return (char)((c + 'A' - akey) % 26 - 'A');
+		
+		 if (Character.isLowerCase(c) )
+			 return (char)((c + 'a' - akey) % 26 - 'a');
+		
+		 return c;
+	}
+
 }
--- CIS500/Project2/CipherTest.java
+++ CIS500/Project2/CipherTest.java
@@ -1,41 +1,58 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Scanner;
 
-/**
-   This program encrypts a file, using the Caesar cipher.
-*/
-public class CipherTest
-{  
-   public static void main(String[] args)
-   {  
-      Scanner in = new Scanner(System.in);
-      try
-      {  
-         System.out.print("Input file: ");
-         String inFile = in.next();
-         System.out.print("Output file: ");
-         String outFile = in.next();
-         System.out.print("Encryption key: ");
-         int key = in.nextInt();
-                        
-         InputStream inStream = new FileInputStream(inFile);
-         OutputStream outStream = new FileOutputStream(outFile);
+public class CipherTest {
 
-         Cipher cipher = new Cipher(key);
-         cipher.encryptStream(inStream, outStream);
+	public static void main(String args[]){
+		try{
+			String keyword = "";
+			Scanner in = new Scanner(System.in);
 
-         inStream.close();
-         outStream.close();
-      }
-      catch (IOException exception)
-      {  
-         System.out.println("Error processing file: " + exception);
-      }
-   }
+			
+			System.out.print("Select an option: 1 to encrypt, 2 to decrypt or 0 to quit");
+			int input = in.nextInt();
+			
+			if(input == 1 ){
+				System.out.println("Input encryption keyword: ");
+				keyword = in.next();
+				InputStream inStream = new FileInputStream("data.txt");
+				OutputStream outEStream = new FileOutputStream("data.encrypted");
+
+				// Encryption Process
+				Cipher cipher = new Cipher(keyword);
+				cipher.keyConvert();
+				cipher.encryptStream(inStream, outEStream);
+				inStream.close();
+				outEStream.close();
+			} else if(input == 2){
+				System.out.println("Input decryption keyword: ");
+				keyword = in.next();
+				InputStream inStream = new FileInputStream("data.encrypted");
+				OutputStream outDStream = new FileOutputStream("data.decrypted");
+				
+				// Decryption Process
+				Cipher cipher = new Cipher(keyword);
+				cipher.keyConvert();
+				cipher.decryptStream(inStream, outDStream);
+				inStream.close();
+				outDStream.close();
+				
+			} else if(input == 0){
+				System.out.println("Option 0 selected...");
+				System.out.println("Quitting...");
+				System.exit(0);
+			}
+		} catch(InputMismatchException e){
+			System.out.println("Invalid Input...");
+			System.out.println("Quitting...");
+		} catch(IOException e){
+			System.out.println("Error processing file: " + e);
+		}
+	}
 }
-

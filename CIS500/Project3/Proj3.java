f1d46e0d910b5be47d4364ee29d31b0d8b97e5c2
Victor <Victor@Victors-Macbook-Pro.local>
2016-11-01 18:06:49 -0400

    Project 3 start

--- CIS500/Project3/Proj3.java
+++ CIS500/Project3/Proj3.java
@@ -1,16 +1,24 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+import javafx.scene.shape.Line;
 import java.io.IOException;
 import java.util.Scanner;
-
-import com.sun.javafx.collections.MappingChange.Map;
+import java.util.Set;
 
 public class Proj3 {
 	// TODO Change parseText method to check for identifier 
 	// Get rid of all updateCounts() method to accommodate for inserting the identifiers and their line numbers into the map
 	// Use map to to maintain an ordered collection for identifier 
 	
-	Map<String, Integer> Identifier;
+	HashMap<String, Integer> Identifier;
+	Scanner in = new Scanner(System.in);
 	
-	
+	public Proj3(){
+		Identifier = new HashMap<String, Integer>();
+	}
 	
 	public void parseText (Scanner in)throws IOException{  
 		String token = "";
@@ -75,7 +83,17 @@ public class Proj3 {
 		in.close(); 
 	}
 	
+	public void displayIdentifiers (){
+		Set set = Identifier.entrySet();
+		Iterator i = set.iterator();
+		while(i.hasNext()){
+			Map.Entry me = (Map.Entry)i.next();
+			System.out.println("Identifier: " + me.getKey() + " \t\t & Line Number: " + me.getValue());
+		}
+	}
+	
 	public static void main(String[] args) {
 		//TODO Complete project 3
+		System.out.println("Project 3");
 	}
 }

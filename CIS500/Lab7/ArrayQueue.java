e3e7966b47b368216acbc365fb7dfd476867c679
Victor <Victor@Victors-Macbook-Pro.local>
2016-11-17 19:05:45 -0500

    Lab6

---
+++ CIS500/Lab7/ArrayQueue.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+public class ArrayQueue<T> { 
+
+	private ArrayList<T> list; 
+
+	public ArrayQueue() { // instantiate the queue/list
+		list = new ArrayList<T>();
+	} 
+
+	// returns true if the queue is empty, false otherwise 
+	public boolean isEmpty() { 
+		if(list.isEmpty()){
+			return true;
+		} else {
+			return false;
+		}
+	}
+ 
+	// adds the element to the end of the queue 
+	public void enQueue(T element) { 
+			list.add(element);
+			System.out.println("Added " + element + " to the queue.");
+	}
+ 
+	// removes the element at the front of the queue and returns it
+	// Throws EmptyQueueException if queue is empty
+	public T deQueue() throws IndexOutOfBoundsException { 
+		if(list.isEmpty()){	
+			// Need to figure out how to throw EmptyQueueException
+			throw new IndexOutOfBoundsException("EmptyQueueException");
+		} else {
+			System.out.println("Removed " + list.get(0) + " from the queue.");
+			list.remove(0);
+		}
+		return null;
+	}
+	
+	// returns a String representation of the queue, which shows 
+	// a list of elements in the queue
+	public String toString() {
+		System.out.println(list);
+		return null;
+//		return "Array list values: " + list.toString();
+	}
+
+	// Removes all occurrences of element from the queue 
+	public void removeAll(T element) { 
+		list.clear();
+	}
+}	
+
---
+++ CIS500/Lab7/QueueDemo.java
@@ -0,0 +1,41 @@
+
+public class QueueDemo {
+
+	public static void main(String[] args) {
+		// create a queue of strings
+		ArrayQueue<String> aQ = new ArrayQueue<String>();
+		// add three strings to the queue
+		aQ.enQueue("ABC");
+		aQ.enQueue("DEF");
+		aQ.enQueue("GHI");
+		// remove two strings from the queue
+		aQ.deQueue();
+		aQ.deQueue();
+		// display all elements in the queue
+		aQ.toString();
+		// add two strings from the queue
+		aQ.enQueue("JKL");
+		aQ.enQueue("MNO");
+		// remove one string from the queue
+		aQ.deQueue();
+		// display the list of strings in the queue
+		aQ.toString();
+		System.out.println("");
+		
+		
+		
+		// create a queue of coins
+		ArrayQueue<Coin> coins = new ArrayQueue<Coin>();
+		// add three coins to the queue
+		Coin p = new Coin(0.01, "Penny");
+		Coin n = new Coin(0.05, "Nickel");
+		Coin d = new Coin(0.10, "Dime");
+		coins.enQueue(p);
+		coins.enQueue(n);
+		coins.enQueue(d);
+		// remove one coin from the queue
+		coins.deQueue();
+		//display the list of coins in the queue
+		coins.toString();
+	}
+}

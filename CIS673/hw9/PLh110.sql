1e8aff332e1a639cad035f0f47992ab38fb5b1c0
Victor Sun <sunvi@eos05.cis.gvsu.edu>
2017-11-27 14:40:00 -0500

    V

--- CIS673/hw9/PLh110.sql
+++ CIS673/hw9/PLh110.sql
@@ -1,29 +1,17 @@
-/*
-This trigger implements part of the inclusion dependency bIC5 (see our
-Sailors schema).It verifies, upon DELETE, or UPDATE on the Sailors table,
-that boats.logKeeper is in Sailors.trainee
-*/
--- Notice the use of PRAGMA AUTONOMOUS_TRANSACTION
--- -------------------------------------------------------
--- File: PLh110.sql
--- Author: Victor Sun
---
 CREATE OR REPLACE TRIGGER bIC5_TB
 BEFORE DELETE OR UPDATE OF trainee ON Sailors
 FOR EACH ROW
 DECLARE
 	PRAGMA AUTONOMOUS_TRANSACTION; -- You’ll need this directive
---
--- <<< YOUR CODE GOES HERE>
---
 BEGIN
-	SELECT COUNT(*)
-	FROM SAILORS S, BOATS B
-	WHERE S.TRAINEE = B.LOGKEEPER AND S.TRAINEE = :OLD.trainee;
-	IF numFOUND > 0	THEN
-		RAISE_APPLICATION_ERROR (-20001||'+++++ DELETE or UPDATE rejected. Trainee ..'||S.TRAINEE||'..is a LogKeeper')
-	END IF;
+	FOR E IN (SELECT S.TRAINEE, COUNT(*)
+		FROM SAILORS S, BOATS B
+		WHERE B.LOGKEEPER = :OLD.SID
+		GROUP BY S.TRAINEE
+		HAVING COUNT(*) > 0)
+	LOOP
+		RAISE_APPLICATION_ERROR (-20001,'+++++ DELETE or UPDATE rejected. Trainee ..'||:OLD.SID||'.. is a LogKeeper');
+	END LOOP E;
 END;
 /
 SHOW ERROR
--- -------------------------------------------------------
